2f9e64349f4ec62a2c94e84c9283f10c86e2618a
student.lozin <lozindenis6@gmail.com>
2022-10-01 10:31:22 +0300

    Create work "№4"

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 start = int(input("Введіть початкове число - "))
+end = int(input("Введіть кінцеве число - "))
+temp = start
+start = end
+end = temp
+while start>end:
+    start-=1
+    print(f"{start}")
